5d7c8bf85fe57bb42b9c4ca3daf60999efec4d0c
Carlos Avalos Soto <carlos.avalos0409@gmail.com>
2023-03-31 15:56:10 -0600

    Ya solo falta viewcontroller

--- BuscaminasV1/BuscaminasCollectionViewCell.swift
+++ BuscaminasV1/BuscaminasCollectionViewCell.swift
@@ -8,6 +8,53 @@
 import UIKit
 
 class BuscaminasCollectionViewCell: UICollectionViewCell {
+    var celda: Celdas?
+    @IBOutlet weak var imvCelda: UIImageView!
+    @IBOutlet weak var imvOverCelda: UIImageView!
+    
+    func setearCeldas(campo: Celdas) {
+        celda = campo;
+        
+        imvOverCelda.image = UIImage(named: "cellover")
+        imvCelda.image = UIImage(named: (celda?.imageName)!)
+        imvCelda.alpha = 1
+        
+        // Ubicar las celdas que han sido descubiertas
+        if (celda?.isOpen)! {
+            imvOverCelda.alpha = 0
+        }
+        else {
+            imvOverCelda.alpha = 1
+        }
+    }
+    
+    // Colocar banderas sobre las celdas
+    func flag() {
+        imvOverCelda.image = UIImage(named: "flag")
+        if !(celda?.mina)! {
+            imvCelda.image = UIImage(named: "wrongflag")
+        }
+    }
+    
+    // Quitar una bandera sobre una celda
+    func unflag() {
+        imvOverCelda.image = UIImage(named: "cellover")
+        if !(celda?.mina)! {
+            imvCelda.image = UIImage(named: (celda?.imageName)!)
+        }
+    }
+    
+    // Descubrir una celda
+    func descubrirCelda() {
+        imvOverCelda.alpha = 0
+    }
+    
+    // Explosión de minas
+    func explosion() {
+        imvCelda.image = UIImage(named: "explodedmine")
+        celda?.imageName = "explodedmine"
+        descubrirCelda()
+    }
     
     
 }
