58052d80c7803caa62d718171171499e02f3a4a7
Pablo <pabloalvaradovazquez10@gmail.com>
2023-04-13 16:24:58 -0600

    push

---
+++ BuscaminasV1/RecordsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  RecordsViewController.swift
+//  BuscaminasV1
+//
+//  Created by imac on 13/04/23.
+//
+
+import UIKit
+
+class RecordsViewController: UIViewController {
+
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func `return`(_ sender: Any)
+    {
+        self.dismiss(animated: true)
+
+    }
+    
+    
+}
