7042e82f61a840f6a754aed8724dc66a722c9d19
Pablo <pabloalvaradovazquez10@gmail.com>
2023-04-16 20:05:48 -0600

    push

--- BuscaminasV1/RecordsTableViewController.swift
+++ BuscaminasV1/RecordsTableViewController.swift
@@ -16,7 +16,7 @@ class RecordsTableViewController: UITableViewController {
         sortedJugadoresTiempo = records.jugadoresTiempo.sorted(by: { $0.value < $1.value })
         records.jugadoresTiempo = Dictionary(uniqueKeysWithValues: sortedJugadoresTiempo)
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: "celda")
-
+        tableView.separatorStyle = .none // Quia los bordes de la tabla
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
@@ -39,7 +39,10 @@ class RecordsTableViewController: UITableViewController {
         let tiempo = sortedJugadoresTiempo[indexPath.row].value
         
         cell.textLabel?.text = nombre + " - " +  String(tiempo) + " s"
-                
+        cell.textLabel?.font = UIFont.systemFont(ofSize: 26)
+        cell.textLabel?.textAlignment = .center // Alinea el texto al centro
+        
+        cell.backgroundColor = UIColor(red: 0.9, green: 0.9, blue: 0.9, alpha: 1.0) // Cambia el fondo de la celda a gris claro
         return cell
     }
 }
