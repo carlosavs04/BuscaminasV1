1f8677e0d309fa677fb7d5037976db907f048f1d
Pablo <pabloalvaradovazquez10@gmail.com>
2023-04-17 12:45:32 -0600

    nn

--- BuscaminasV1/Records.swift
+++ BuscaminasV1/Records.swift
@@ -11,7 +11,8 @@ class Records: NSObject {
     var jugadoresTiempo: [String: Float]
     static var records: Records!
     
-    override init() {
+    override init()
+    {
         jugadoresTiempo = ["Carlos": 50.00, "Pablo": 60.00, "Jhon": 70.00, "James": 80.00, "Peter": 90.00]
     }
     
